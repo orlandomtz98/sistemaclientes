566706f80a0d37323398cf40f86199a598ae5510
orlandomtz98 <orlando_fut@hotmail.com>
2020-08-11 19:51:26 -0500

    modificacion

--- application/views/ventasRealizadas.php
+++ application/views/ventasRealizadas.php
@@ -20,9 +20,16 @@
   <!-- Page level plugins -->
   <script src="assets/datatables/jquery.dataTables.min.js"></script>
   <script src="assets/datatables/dataTables.bootstrap4.min.js"></script>
-
+  <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/dataTables.buttons.min.js"></script>
+      <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/buttons.flash.min.js"></script>
+      <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jszip.min.js"></script>
+      <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/vfs_fonts.js"></script>
+      <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/pdfmake.min.js"></script>
+      <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/buttons.html5.min.js"></script>
+      <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/buttons.print.min.js"></script>
   <!-- Page level custom scripts -->
   <script src="assets/js/demo/datatables-demo.js"></script>
+  
   <title>Ventas realizadas</title>
 
   <!-- Custom fonts for this template -->
@@ -228,6 +235,9 @@
               <h6 class="m-0 font-weight-bold text-primary">Ventas realizadas</h6>
             </div>
             <div class="card-body">
+              <h4>Filtrar por fecha</h4>
+                    Desde: <input id="min" type="text" name="min" placeholder="aaaa-mm-dd"><br><br>
+                    Hasta: <input id="max" type="text" name="max" placeholder="aaaa-mm-dd">
               <div class="table-responsive">
                 <table class="table table-bordered" id="ventas" width="100%" cellspacing="0">
                   <thead>
@@ -236,7 +246,7 @@
                       <th>Fecha</th>
                       <th>Producto</th>
                       <th>Cantidad</th>
-                      <th>Precio</th>
+                      <th>$ Total de la venta</th>
                     </tr>
                   </thead>
                   
@@ -314,11 +324,59 @@
 </html>
 
 
-  <script>
-    $(document).ready( function () {
-    $('#ventas').DataTable();
-} );
-  </script>
+<script>
+  $.fn.dataTableExt.afnFiltering.push(
+    function( oSettings, aData, iDataIndex ) {
+        var iFini = document.getElementById('min').value;
+        var iFfin = document.getElementById('max').value;
+        var iStartDateCol = 1;
+        var iEndDateCol = 1;
+ 
+        iFini=iFini.substring(6,10) + iFini.substring(3,5)+ iFini.substring(0,2);
+        iFfin=iFfin.substring(6,10) + iFfin.substring(3,5)+ iFfin.substring(0,2);
+ 
+        var datofini=aData[iStartDateCol].substring(6,10) + aData[iStartDateCol].substring(3,5)+ aData[iStartDateCol].substring(0,2);
+        var datoffin=aData[iEndDateCol].substring(6,10) + aData[iEndDateCol].substring(3,5)+ aData[iEndDateCol].substring(0,2);
+ 
+        if ( iFini === "" && iFfin === "" )
+        {
+            return true;
+        }
+        else if ( iFini <= datofini && iFfin === "")
+        {
+            return true;
+        }
+        else if ( iFfin >= datoffin && iFini === "")
+        {
+            return true;
+        }
+        else if (iFini <= datofini && iFfin >= datoffin)
+        {
+            return true;
+        }
+        return false;
+    }
+);
+
+  $(document).ready(function() {
+      var table = $('#ventas').DataTable({
+         dom: 'pBf',
+       
+        buttons: [
+              {
+                extend: 'excel',
+                text: 'Exportar a Excel',
+                title: 'Reporte de ventas'
+            },
+        ]
+       
+    });
+ 
+      // Add event listeners to the two range filtering inputs
+      $('#min').keyup( function() { table.draw(); } );
+      $('#max').keyup( function() { table.draw(); } );
+  } );
+</script>
 <style>
     th,#fechaTabla,#productoTabla,#cantidadTabla,#precioTabla{
       text-align: center;
--- sistemaclientes.sql
+++ sistemaclientes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 27-07-2020 a las 17:46:44
+-- Tiempo de generación: 11-08-2020 a las 19:50:07
 -- Versión del servidor: 5.7.24
 -- Versión de PHP: 7.2.14
 
